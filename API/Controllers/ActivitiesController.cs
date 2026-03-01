427e4e9a5e97a01aca9df89b8d77717b9faf2f9c
Alexandr <sanoktabx@gmail.com>
2026-03-01 17:57:21 +0500

    First commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace FullstackApp.Controllers;
+
+
+[ApiController]
+[Route("/api/[controller]")]
+public class ActivitiesController(AppDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        var data = await context.Activities.ToListAsync();
+        return Ok(data);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivity(string id)
+    {
+        var data = await context.Activities.FindAsync(id);
+
+        if (data == null) return NotFound();
+        
+        return data;
+    }
+}
